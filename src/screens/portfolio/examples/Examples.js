10fa9725fee9e2866b0daf3dc7c38e83e1d41394
ronaldoronaldo <dataweiss@gmail.com>
2020-03-31 00:32:20 -0300

    Examples.js

--- src/screens/portfolio/examples/Examples.js
+++ src/screens/portfolio/examples/Examples.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import {
+  ComponentsSection,
+  DesktopScreens,
+  ExamplesContainer,
+  ResponsiveScreens,
+  SectionTitle
+} from './Examples.style'
 
 const Examples = () => {
   return (
-    <>
+    <ExamplesContainer>
+      <ResponsiveScreens>
+        <SectionTitle>Responsive Screens</SectionTitle>
 
-    </>
+      </ResponsiveScreens>
+      <DesktopScreens>
+        <SectionTitle>Desktop Screens</SectionTitle>
+
+      </DesktopScreens>
+      <ComponentsSection>
+        <SectionTitle>Components Examples</SectionTitle>
+
+      </ComponentsSection>
+    </ExamplesContainer>
   )
 }
 
--- src/screens/portfolio/examples/Examples.style.js
+++ src/screens/portfolio/examples/Examples.style.js
@@ -1,5 +1,39 @@
 import styled from 'styled-components'
+import {colors, fontWeights, sizes} from 'config/ui'
 
-export const Container = styled.div`
+export const ExamplesContainer = styled.div`
+  color: ${colors.black4};
+  position: relative;
+  width: 100%;
+  top: 134px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: ${sizes.mdScreen}px) {
+    top: 212px;
+  } 
+`
 
+export const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`
+
+export const ResponsiveScreens = styled(Section)`
+`
+export const DesktopScreens = styled(Section)`
+  display: none;
+  @media (min-width: ${sizes.mdScreen}px) {
+    display: flex;
+  } 
+`
+export const ComponentsSection = styled(Section)`
+`
+
+export const SectionTitle = styled.span`
+  font-size: 32px;
+  font-weight: ${fontWeights.bold};
 `
