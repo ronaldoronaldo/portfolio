3c3d4ee566a3ccf7ef21465148a2d30c096c4028
ronaldoronaldo <dataweiss@gmail.com>
2020-04-04 21:25:28 -0300

    style

--- src/screens/portfolio/about-me/AboutMe.js
+++ src/screens/portfolio/about-me/AboutMe.js
@@ -5,10 +5,24 @@ import Badge from 'components/portfolio/badge/Badge'
 
 import reactHooks from 'assets/images/portfolio/badges/reactHooks.svg'
 import graphql from 'assets/images/portfolio/badges/graphql.svg'
-import jest from 'assets/images/portfolio/badges/jest.svg'
+// import jest from 'assets/images/portfolio/badges/jest.svg'
 import mobileFirst from 'assets/images/portfolio/badges/mobileFirst.svg'
+import uxui from 'assets/images/portfolio/badges/uxui.svg'
+import chart3 from 'assets/images/portfolio/badges/chart3.svg'
+import maps from 'assets/images/portfolio/badges/maps.svg'
+// import rRouter from 'assets/images/portfolio/badges/reactRouter.svg'
+import enzyme from 'assets/images/portfolio/badges/test1.svg'
+// import testdriven from 'assets/images/portfolio/badges/teste2.svg'
+// import linuxXP from 'assets/images/portfolio/badges/code1.svg'
+// import SRRCode from 'assets/images/portfolio/badges/code2.svg'
+import responsive from 'assets/images/portfolio/badges/responsive2.svg'
 
 const badges = [
+  {
+    title: 'Mobile First',
+    image: mobileFirst,
+    iconWidth: 84
+  },
   {
     title: 'React Hooks',
     image: reactHooks,
@@ -19,16 +33,56 @@ const badges = [
     image: graphql,
     iconWidth: 76
   },
+  // {
+  //   title: 'Jest',
+  //   image: jest,
+  //   iconWidth: 74
+  // },
   {
-    title: 'Jest',
-    image: jest,
-    iconWidth: 74
+    title: 'UX / UI',
+    image: uxui,
+    iconWidth: 76
   },
   {
-    title: 'Mobile First',
-    image: mobileFirst,
-    iconWidth: 84
-  }
+    title: 'Charts',
+    image: chart3,
+    iconWidth: 76
+  },
+  {
+    title: 'Maps',
+    image: maps,
+    iconWidth: 76
+  },
+  // {
+  //   title: 'React Router',
+  //   image: rRouter,
+  //   iconWidth: 76
+  // },
+  // {
+  //   title: 'Enzyme',
+  //   image: enzyme,
+  //   iconWidth: 76
+  // },
+  {
+    title: 'Test Driven',
+    image: enzyme,
+    iconWidth: 76
+  },
+  // {
+  //   title: 'SRR Code',
+  //   image: SRRCode,
+  //   iconWidth: 76
+  // },
+  {
+    title: 'Responsive',
+    image: responsive,
+    iconWidth: 76
+  },
+  // {
+  //   title: 'Linux OS',
+  //   image: linuxXP,
+  //   iconWidth: 76
+  // }
 ]
 
 const AboutMe = () => {
